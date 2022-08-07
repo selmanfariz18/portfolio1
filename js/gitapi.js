a26b03c23b016f57f6f2f065ebd2a9e157fffe28
selmanfariz18 <salmanulfarizyka@gmail.com>
2022-08-07 10:43:55 +0530

    edits is api.js

--- js/gitapi.js
+++ js/gitapi.js
@@ -2,36 +2,36 @@
 async function getData(){
     const res=await fetch('https://api.github.com/users/selmanfariz18')
     const data=await res.json()
-
+    
     const card5 =document.getElementById('stat1')
     const name =document.createElement('h3')
     name.textContent=data.name
 
     card5.appendChild(name)
 
-    const repo=await fetch('https://api.github.com/users/selmanfariz18/repos')
+    const repo=await fetch('https://api.github.com/users/selmanfariz18')
     const data1=await repo.json()
     console.log(data1)
     const card =document.getElementById('stat2')
     const repo_name =document.createElement('h3')
-    repo_name.textContent= data1.length
+    repo_name.textContent= data1.public_repos
     card.appendChild(repo_name)
 
-    const folo=await fetch('https://api.github.com/users/selmanfariz18/followers')
+    const folo=await fetch('https://api.github.com/users/selmanfariz18')
     const data2=await folo.json()
 
     const card2 =document.getElementById('stat3')
     const folo_name =document.createElement('h3')
-    folo_name.textContent= data2.length
+    folo_name.textContent= data2.followers
 
     card2.appendChild(folo_name)
 
-    const folow=await fetch('https://api.github.com/users/selmanfariz18/following')
+    const folow=await fetch('https://api.github.com/users/selmanfariz18')
     const data3=await folow.json()
 
     const card3 =document.getElementById('stat4')
     const folow_name =document.createElement('h3')
-    folow_name.textContent= data3.length
+    folow_name.textContent= data3.following
 
     card3.appendChild(folow_name)
 
